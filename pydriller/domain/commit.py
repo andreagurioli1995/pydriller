1a355dcc4ff8e51eb6edfd527c70cd9e81bf61dc
andreagurioli1995 <38469046+andreagurioli1995@users.noreply.github.com>
2022-10-12 14:26:37 +0200

    changing decoding on commit.py

--- pydriller/domain/commit.py
+++ pydriller/domain/commit.py
@@ -168,10 +168,10 @@ class ModifiedFile:
         self._new_path = Path(new_path) if new_path is not None else None
         self.change_type = change_type
         self.diff = diff_and_sc["diff"]
-        self.__source_code = diff_and_sc["source_code"]
-        self.__source_code_before = diff_and_sc["source_code_before"]
-        self.content = diff_and_sc["content"]
-        self.content_before = diff_and_sc["content_before"]
+        # self.__source_code = diff_and_sc["source_code"]
+        # self.__source_code_before = diff_and_sc["source_code_before"]
+        # self.content = diff_and_sc["content"]
+        # self.content_before = diff_and_sc["content_before"]
 
         self._nloc = None
         self._complexity = None
@@ -725,10 +725,10 @@ class Commit:
 
             diff_and_sc = {
                 "diff": self._get_decoded_str(diff.diff),
-                "source_code_before": self._get_decoded_sc_str(diff.a_blob),
-                "source_code": self._get_decoded_sc_str(diff.b_blob),
-                "content_before": self._get_decoded_sc_str(diff.a_blob),
-                "content": self._get_decoded_sc_str(diff.b_blob),
+                # "source_code_before": self._get_decoded_sc_str(diff.a_blob),
+                # "source_code": self._get_decoded_sc_str(diff.b_blob),
+                # "content_before": self._get_decoded_sc_str(diff.a_blob),
+                # "content": self._get_decoded_sc_str(diff.b_blob),
             }
 
             modified_files_list.append(
@@ -739,23 +739,25 @@ class Commit:
 
     def _get_decoded_str(self, diff):
         try:
+            print("inside")
             return diff.decode("utf-8", "ignore")
         except (AttributeError, ValueError):
+            print("alpha error")
             logger.debug(
                 "Could not load the diff of a " "file in commit %s",
                 self._c_object.hexsha,
             )
             return None
 
-    def _get_decoded_sc_str(self, diff):
-        try:
-            return diff.data_stream.read().decode("utf-8", "ignore")
-        except (AttributeError, ValueError):
-            logger.debug(
-                "Could not load source code of a " "file in commit %s",
-                self._c_object.hexsha,
-            )
-            return None
+    # def _get_decoded_sc_str(self, diff):
+    #     try:
+    #         return diff.data_stream.read().decode("utf-8", "ignore")
+    #     except (AttributeError, ValueError):
+    #         logger.debug(
+    #             "Could not load source code of a " "file in commit %s",
+    #             self._c_object.hexsha,
+    #         )
+    #         return None
 
     @property
     def in_main_branch(self) -> bool:
